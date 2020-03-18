2f495952ddecf6c8adc28539830d10e1a857b5cc
Patrick Angelo <pat.cameguing@gmail.com>
2020-03-18 12:53:25 +0800

    Unit testing

---
+++ WuxiaWorld.UnitTests/Controller/GenreControllerTest.cs
@@ -0,0 +1,100 @@
+﻿namespace WuxiaWorld.UnitTests.Controller {
+
+    using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
+
+    using BLL.Services.Interfaces;
+
+    using Controllers;
+
+    using DAL.Entities;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using Moq;
+
+    using Xunit;
+
+    public class GenreControllerTest {
+
+        public GenreControllerTest() {
+
+            _mockGenreService = new Mock<IGenreService>();
+            _controller = new GenreController(_mockGenreService.Object);
+        }
+
+        private readonly Mock<IGenreService> _mockGenreService;
+        private readonly GenreController _controller;
+
+        private TaskAwaiter<IActionResult> ControllerGetByName() {
+
+            var controllerGetByName = _controller.Get(It.IsAny<string>()).GetAwaiter();
+            return controllerGetByName;
+        }
+
+        [Fact]
+        public void GetAll_AssertReturningOK() {
+            var controllerGet = _controller.Get().GetAwaiter();
+
+            if (controllerGet.IsCompleted) {
+
+                var result = controllerGet.GetResult();
+
+                Assert.IsType<OkObjectResult>(result);
+            }
+        }
+
+        [Fact]
+        public void GetAll_AssertReturningResult() {
+
+            _mockGenreService.Setup(repo => repo.GetAll())
+                .ReturnsAsync(() => new List<Genres> {
+                    new Genres(),
+                    new Genres()
+                });
+
+            var controllerGet = _controller.Get().GetAwaiter();
+
+            if (controllerGet.IsCompleted) {
+
+                var result = controllerGet.GetResult();
+
+                var viewResult = Assert.IsType<OkObjectResult>(result);
+                var employees = Assert.IsType<List<Genres>>(viewResult.Value);
+                Assert.Equal(2, employees.Count);
+            }
+        }
+
+        [Fact]
+        public void GetByName_AssertReturningOK() {
+
+            var controllerGetByName = ControllerGetByName();
+
+            if (controllerGetByName.IsCompleted) {
+
+                var result = controllerGetByName.GetResult();
+
+                Assert.IsType<OkObjectResult>(result);
+            }
+        }
+
+        [Fact]
+        public void GetByName_AssertReturningResult() {
+
+            _mockGenreService.Setup(repo => repo.GetByName(It.IsAny<string>()))
+                .ReturnsAsync(() => new Genres());
+
+            var controllerGetByName = ControllerGetByName();
+
+            if (controllerGetByName.IsCompleted) {
+
+                var result = controllerGetByName.GetResult();
+
+                var viewResult = Assert.IsType<OkObjectResult>(result);
+                var employees = Assert.IsType<Genres>(viewResult.Value);
+                Assert.Equal(0, employees.GenreId);
+            }
+        }
+    }
+
+}
